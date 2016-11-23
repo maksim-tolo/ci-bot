268c15e44b9a85a373c33d4dd868811e61695654
Maxim <maxim.alekseenko@codex-soft.com>
2016-11-23 11:58:55 +0300

    add base questions

--- src/index.js
+++ src/index.js
@@ -7,10 +7,54 @@ const PORT = process.env.PORT || 3000;
 const connector = new builder.ChatConnector();
 const bot = new builder.UniversalBot(connector);
 
-bot.dialog('/', (session) => {
-  session.send(session.message.text.split('').reverse().join(''));
-});
-
 app.post('/api/messages', connector.listen());
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+
+
+bot.dialog('/', [
+    function (session) {
+        session.send('Hello, dear friend!');
+        session.beginDialog('/ensureJenkinsProfile', session.userData);
+    },
+    function (session, results) {
+        session.userData.profile = results.response;
+        session.send('Hello %(username)s! It is your password - %(password)s! Ha-ha! Url - %(url)s', session.userData.profile);
+    }
+]);
+bot.dialog('/ensureJenkinsProfile', [
+    function (session, args, next) {
+        session.dialogData.profile = args || {};
+        if (!session.dialogData.profile.url) {
+            builder.Prompts.text(session, 'Please, enter Jenkins url?');
+        } else {
+            next();
+        }
+    },
+    function (session, results, next) {
+        if (results.response) {
+            session.dialogData.profile.url = results.response;
+        }
+        if (!session.dialogData.profile.username) {
+            builder.Prompts.text(session, 'Please, enter your username for Jenkins?');
+        } else {
+            next();
+        }
+    },
+    function (session, results, next) {
+        if (results.response) {
+            session.dialogData.profile.username = results.response;
+        }
+        if (!session.dialogData.profile.password) {
+            builder.Prompts.text(session, 'Please, enter the password.');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        if (results.response) {
+            session.dialogData.profile.password = results.response;
+        }
+        session.endDialogWithResult({ response: session.dialogData.profile });
+    }
+]);
